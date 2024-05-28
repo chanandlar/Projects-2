eda29f8868aa5474a29a66497639c5bcab6c8e11
Shoeb Ahmed Khan <shoebkhan100100@gmail.com>
2024-05-28 11:52:43 +0530

    initial commit

---
+++ Module6-JPAandHibernate/src/main/java/com/example/springboot/Module6_JPAandHibernate/Module6JpAandHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.Module6_JPAandHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module6JpAandHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module6JpAandHibernateApplication.class, args);
+	}
+
+}
---
+++ Module6-JPAandHibernate/src/test/java/com/example/springboot/Module6_JPAandHibernate/Module6JpAandHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.Module6_JPAandHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module6JpAandHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
